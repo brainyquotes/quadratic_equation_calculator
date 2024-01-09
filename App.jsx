16cf0bfb754aec0636e1bb2f88360f61f322943f
Emmanuel Nwobueze <38289623+brainyquotes@users.noreply.github.com>
2024-01-08 17:05:02 -0800

    Create App.jsx
    The App Component which is the parent to QuadraticForm component

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+/* importing components */
+import QuadraticForm from './components/QuadraticForm';
+
+/*importing the app css file */
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div>    
+      <QuadraticForm />
+    </div>
+  )
+}
+
+export default App
